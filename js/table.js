3b82758ee9083f944ca916cde8d9a4804bcc9040
EnzoBomark <enzo.bomark@chasacademy.se>
2020-12-19 15:15:17 +0100

    Update table.js

--- js/table.js
+++ js/table.js
@@ -6,42 +6,47 @@
      cells[i].onclick = function () {
          if (this.hasAttribute('data-clicked')) return;
 
-         this.setAttribute('data-clicked', 'true');
-         this.setAttribute('data-text', this.innerHTML);
+         //  this.setAttribute('data-clicked', 'true');
+         //  this.setAttribute('data-text', this.innerHTML);
 
-         let input = document.createElement('input');
-         //         input.setAttribute('type', 'text');
-         //         input.value = this.innerHTML;
-         //         input.style.width = this.offsetWidth + "px";
-         //         input.style.height = this.offsetHeight + "px";
+         //  let input = document.createElement('input');
+         //  input.setAttribute('type', 'text');
+         //  input.value = this.innerHTML;
+         //  input.style.width = this.offsetWidth + "px";
+         //  input.style.height = this.offsetHeight + "px";
 
-         //         input.onblur = function () {
-         //             let td = input.parentElement;
-         //             let orig_text = input.parentElement.getAttribute('data-text');
-         //             let current_text = this.value;
+         //  input.onblur = function () {
+         //      let td = input.parentElement;
+         //      let orig_text = input.parentElement.getAttribute('data-text');
+         //      let current_text = this.value;
 
-         //             if (orig_text !== current_text) {
-         //                 //Save data here.
-         //                 td.removeAttribute('data-clicked');
-         //                 td.removeAttribute('data-text');
-         //                 td.innerHTML = current_text;
-         //                 console.log(orig_text + ' is change to ' + current_text);
-         //             } else {
-         //                 td.removeAttribute('data-clicked');
-         //                 td.removeAttribute('data-text');
-         //                 td.innerHTML = orig_text;
-         //                 console.log('no change');
-         //             }
-         //         }
+         //      if (orig_text !== current_text) {
+         //          //Save data here.
+         //          td.removeAttribute('data-clicked');
+         //          td.removeAttribute('data-text');
+         //          td.innerHTML = current_text;
+         //          console.log(orig_text + ' is change to ' + current_text);
+         //      } else {
+         //          td.removeAttribute('data-clicked');
+         //          td.removeAttribute('data-text');
+         //          td.innerHTML = orig_text;
+         //          console.log('no change');
+         //      }
+         //  }
 
-         //         input.onkeypress = function () {
-         //             if (event.keyCode == 13) {
-         //                 this.blur();
-         //             }
-         //         }
+         //  this.innerHTML = '';
+         //  this.append(input);
+         //  this.firstElementChild.select();
 
-         //this.innerHTML = '';
-         //this.append(input);
-         //this.firstElementChild.select();
+         let orgText = this.innerHTML;
+
+         this.onblur = function () {
+
+             if (orgText !== this.innerHTML) {
+                 console.log(this.innerHTML);
+             } else {
+                 console.log('no change');
+             }
+         }
      }
  }
